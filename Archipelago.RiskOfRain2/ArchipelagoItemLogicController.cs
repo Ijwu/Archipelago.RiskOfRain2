965d3eff50cf98e40d0f764b5b007ad80d5938c8
Hussein Farran <hmfarran@gmail.com>
2021-09-16 17:42:08 -0400

    Prevent certain drops from affecting location check progress.
    Fix lunar equipment grants.

--- Archipelago.RiskOfRain2/ArchipelagoItemLogicController.cs
+++ Archipelago.RiskOfRain2/ArchipelagoItemLogicController.cs
@@ -33,6 +33,7 @@ namespace Archipelago.RiskOfRain2
         private Dictionary<int, string> itemLookupById;
         private Dictionary<int, string> locationLookupById;
         private GameData riskOfRainData;
+        private PickupIndex[] skippedItems;
 
         private GameObject smokescreenPrefab;
 
@@ -52,6 +53,37 @@ namespace Archipelago.RiskOfRain2
             session.PacketReceived += Session_PacketReceived;
 
             smokescreenPrefab = Resources.Load<GameObject>("Prefabs/Effects/SmokescreenEffect").InstantiateClone("LocationCheckPoof", true);
+
+            skippedItems = new PickupIndex[]
+            {
+                PickupCatalog.FindPickupIndex(RoR2Content.Equipment.AffixBlue.equipmentIndex),
+                PickupCatalog.FindPickupIndex(RoR2Content.Equipment.AffixEcho.equipmentIndex),
+                PickupCatalog.FindPickupIndex(RoR2Content.Equipment.AffixGold.equipmentIndex),
+                PickupCatalog.FindPickupIndex(RoR2Content.Equipment.AffixHaunted.equipmentIndex),
+                PickupCatalog.FindPickupIndex(RoR2Content.Equipment.AffixLunar.equipmentIndex),
+                PickupCatalog.FindPickupIndex(RoR2Content.Equipment.AffixPoison.equipmentIndex),
+                PickupCatalog.FindPickupIndex(RoR2Content.Equipment.AffixRed.equipmentIndex),
+                PickupCatalog.FindPickupIndex(RoR2Content.Equipment.AffixWhite.equipmentIndex),
+                PickupCatalog.FindPickupIndex(RoR2Content.Equipment.AffixYellow.equipmentIndex),
+                PickupCatalog.FindPickupIndex("LunarCoin.Coin0"),
+                PickupCatalog.FindPickupIndex(RoR2Content.Items.ArtifactKey.itemIndex),
+                PickupCatalog.FindPickupIndex(RoR2Content.Artifacts.Bomb.artifactIndex),
+                PickupCatalog.FindPickupIndex(RoR2Content.Artifacts.Command.artifactIndex),
+                PickupCatalog.FindPickupIndex(RoR2Content.Artifacts.EliteOnly.artifactIndex),
+                PickupCatalog.FindPickupIndex(RoR2Content.Artifacts.Enigma.artifactIndex),
+                PickupCatalog.FindPickupIndex(RoR2Content.Artifacts.FriendlyFire.artifactIndex),
+                PickupCatalog.FindPickupIndex(RoR2Content.Artifacts.Glass.artifactIndex),
+                PickupCatalog.FindPickupIndex(RoR2Content.Artifacts.MixEnemy.artifactIndex),
+                PickupCatalog.FindPickupIndex(RoR2Content.Artifacts.MonsterTeamGainsItems.artifactIndex),
+                PickupCatalog.FindPickupIndex(RoR2Content.Artifacts.RandomSurvivorOnRespawn.artifactIndex),
+                PickupCatalog.FindPickupIndex(RoR2Content.Artifacts.Sacrifice.artifactIndex),
+                PickupCatalog.FindPickupIndex(RoR2Content.Artifacts.ShadowClone.artifactIndex),
+                PickupCatalog.FindPickupIndex(RoR2Content.Artifacts.SingleMonsterType.artifactIndex),
+                PickupCatalog.FindPickupIndex(RoR2Content.Artifacts.Swarms.artifactIndex),
+                PickupCatalog.FindPickupIndex(RoR2Content.Artifacts.TeamDeath.artifactIndex),
+                PickupCatalog.FindPickupIndex(RoR2Content.Artifacts.WeakAssKnees.artifactIndex),
+                PickupCatalog.FindPickupIndex(RoR2Content.Artifacts.WispOnDeath.artifactIndex),
+            };
         }
 
         private void Session_PacketReceived(ArchipelagoPacketBase packet)
@@ -159,7 +191,15 @@ namespace Archipelago.RiskOfRain2
                     break;
                 case "Lunar Item":
                     var lunar = Run.instance.availableLunarDropList.Choice();
-                    GiveItemToPlayers(lunar);
+                    var pickupDef = PickupCatalog.GetPickupDef(lunar);
+                    if (pickupDef.itemIndex != ItemIndex.None)
+                    {
+                        GiveItemToPlayers(lunar);
+                    }
+                    else if (pickupDef.equipmentIndex != EquipmentIndex.None)
+                    {
+                        GiveEquipmentToPlayers(lunar);
+                    }
                     break;
                 case "Equipment":
                     var equipment = Run.instance.availableEquipmentDropList.Choice();
@@ -237,6 +277,12 @@ namespace Archipelago.RiskOfRain2
 
         private void PickupDropletController_CreatePickupDroplet(On.RoR2.PickupDropletController.orig_CreatePickupDroplet orig, PickupIndex pickupIndex, Vector3 position, Vector3 velocity)
         {
+            if (skippedItems.Contains(pickupIndex))
+            {
+                orig(pickupIndex, position, velocity);
+                return;
+            }
+
             // Run `HandleItemDrop()` first so that the `PickedUpItemCount` is incremented by the time `ItemDropProcessed()` is called.
             var spawnItem = finishedAllChecks || HandleItemDrop();
             
