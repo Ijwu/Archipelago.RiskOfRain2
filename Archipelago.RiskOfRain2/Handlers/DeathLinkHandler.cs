af9e94c5fee1ab3fc5e099eb88cadcf1659e2ad1
Hussein Farran <hmfarran@gmail.com>
2021-11-14 13:27:53 -0500

    Add messages for deathlink.

--- Archipelago.RiskOfRain2/Handlers/DeathLinkHandler.cs
+++ Archipelago.RiskOfRain2/Handlers/DeathLinkHandler.cs
@@ -5,6 +5,7 @@ using System.Text;
 using Archipelago.MultiClient.Net.BounceFeatures.DeathLink;
 using Archipelago.RiskOfRain2.Enums;
 using Archipelago.RiskOfRain2.Extensions;
+using R2API.Utils;
 using RoR2;
 using RoR2.Artifacts;
 using UnityEngine;
@@ -52,6 +53,8 @@ namespace Archipelago.RiskOfRain2.Handlers
 
         private void RunDrizzle(DeathLink dl)
         {
+            ChatMessage.SendColored($"{dl.Source} lost their life. Your deployables end their own in your friend's honor.", Color.red);
+
             Log.LogDebug("Running Drizzle DeathLink");
             var listField = typeof(CharacterMaster).GetField("deployablesList", BindingFlags.NonPublic | BindingFlags.Instance);
             Log.LogDebug($"DeathLink.Drizzle: List Field is null? {listField == null}");
@@ -80,6 +83,8 @@ namespace Archipelago.RiskOfRain2.Handlers
 
         private void RunRainstorm(DeathLink dl)
         {
+            ChatMessage.SendColored($"{dl.Source} lost their life, lose all your money to pay for their funeral.", Color.red);
+
             Log.LogDebug("Running Rainstorm DeathLink");
             var instances = PlayerCharacterMasterController.instances;
             foreach (PlayerCharacterMasterController playerCharacterMaster in instances)
@@ -90,17 +95,22 @@ namespace Archipelago.RiskOfRain2.Handlers
 
         private void RunMonsoon(DeathLink dl)
         {
+            ChatMessage.SendColored($"{dl.Source} lost their life, lose an item to give to their begrieved.", Color.red);
+
             Log.LogDebug("Running Monsoon DeathLink");
             var instances = PlayerCharacterMasterController.instances;
             foreach (PlayerCharacterMasterController playerCharacterMaster in instances)
             {
                 var itemToRemove = playerCharacterMaster.master.inventory.itemAcquisitionOrder.Choice();
+                DirectMessage.SendDirectMessage($"You lost a single ${Language.GetString(ItemCatalog.GetItemDef(itemToRemove).nameToken)}", playerCharacterMaster.networkUser);
                 playerCharacterMaster.master.inventory.RemoveItem(itemToRemove);
             }
         }
 
         private void RunTyphoon(DeathLink dl)
         {
+            ChatMessage.SendColored($"{dl.Source} lost their life, their ghost takes shape and comes back to haunt you.", Color.red);
+
             Log.LogDebug("Running Typhoon DeathLink");
             DoppelgangerInvasionManager.PerformInvasion(new Xoroshiro128Plus(Run.instance.seed));
         }
@@ -109,6 +119,7 @@ namespace Archipelago.RiskOfRain2.Handlers
         {
             Log.LogDebug("AHAHAHAHAHAHAHAHA");
             var randomPlayer = PlayerCharacterMasterController.instances.Choice();
+            ChatMessage.SendColored($"{dl.Source} lost their life, it's only fitting {randomPlayer.GetDisplayName()} loses their own.", Color.red);
             Log.LogDebug($"Selected player {randomPlayer.GetDisplayName()} to die. NetID: {randomPlayer.netId}");
             randomPlayer.master.GetBody().healthComponent.Suicide(damageType: DamageType.VoidDeath);
         }
